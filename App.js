50f9252f383308dc1bfb89bb9f75f0fe8d72cdb7
Mariano Tucat <mariano@skedgo.com>
2017-05-16 21:29:50 -0300

    adding hardcoded places + multiple routing calls + finding faster trip

--- App.js
+++ App.js
@@ -6,25 +6,21 @@ export default class FastGo extends Component {
     super(props);
     this.state = {
       isLoading: true,
-      selectedMode: 'cy_bic',
+      selectedMode: 'pt_pub',
       modes: [],
+      selectedPlaces: 'starbucks',
+      places: {
+        'mcdonalds' : [{"lat": -33.8755296,"lng": 151.2066007, "address": "600 George St"},
+                       {"lat": -33.8730211,"lng": 151.2083184, "address": "Park St & Pitt St"}],
+        'starbucks' : [{"lat": -33.8734138,"lng": 151.209559, "address": "Pacific Power Building, 201 Elizabeth St"},
+                       {"lat": -33.8723705,"lng": 151.2065248, "address": "Queen Victoria Building, 69/455 George St"}],
+      },
       message: 'Selected Mode: ' + 'cy_bic'
     }
   }
 
   componentDidMount() {
-    return fetch('https://bigbang.skedgo.com/satapp-beta/regions.json', {
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json',
-          'X-TripGo-Key': '3ff9fc635afb6187ccf33ccc4610b80a' 
-        },
-        body: JSON.stringify({
-          v: '2',
-        })
-      })
-      .then((response) => response.json())
+      return getRegions()
       .then((regionsJSON) => {
         this.setState({
           isLoading: false,
@@ -48,40 +44,35 @@ export default class FastGo extends Component {
     }
 
     let onButtonPress = () => {
-      this.setState({message: 'Selected Mode: ' + this.state.selectedMode})
-      data = {
-        fromLoc: '(-33.894436,151.110030)',
-        toLoc: '(-33.877294,151.208554)',
-        mode: this.state.selectedMode,
-        wp: '(1,1,1,1)' 
-      }
-      let url = 'https://bigbang.skedgo.com/satapp-beta/routing.json'+ 
-                `?from=${data.fromLoc}&to=${data.toLoc}&modes=${data.mode}&wp=${data.wp}&v=11`
-      return fetch(url, {
-          method: 'GET',
-          headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-            'X-TripGo-Key': '3ff9fc635afb6187ccf33ccc4610b80a' 
-          }
-        })
-        .then((response) => response.json())
+      this.setState({message: 'Selected Mode: ' + this.state.selectedMode + 'Selected Place: ' + this.state.selectedPlaces})
+        return computeFastestTrip(this.state.selectedMode, this.state.places[this.state.selectedPlaces])
         .then((routingJSON) => {
-          console.log(routingJSON);
-          this.setState({
-              message: 'Computed Trips: ' + routingJSON.segmentTemplates[0].action 
-          }, function() {
-            
-          });
+          // console.log(routingJSON);
+          if (routingJSON.hasOwnProperty('error')) {
+            this.setState({
+                message: 'Error: ' + routingJSON.error 
+            }, function() {
+              
+            })
+          } else {
+            this.setState({
+                message: 'Computed Trips: ' + routingJSON.segmentTemplates[0].action 
+            }, function() {
+              
+            })
+          }
         })
         .catch((error) => {
           console.error(error);
         });
     };
 
-
-    let serviceItems = this.state.modes.map( (s, i) => {
+    let modesItems = this.state.modes.map( (s, i) => {
         return <Picker.Item key={i} value={s.mode} label={s.title} />
+    });
+
+    let placesItems = Object.keys(this.state.places).map( (s, i) => {
+        return <Picker.Item key={i} value={s} label={s} />
     });
 
     return (
@@ -89,7 +80,12 @@ export default class FastGo extends Component {
         <Picker
           selectedValue={this.state.selectedMode}
           onValueChange={(mode) => this.setState({selectedMode: mode})}>
-          {serviceItems}
+          {modesItems}
+        </Picker>
+        <Picker
+          selectedValue={this.state.selectedPlaces}
+          onValueChange={(place) => this.setState({selectedPlaces: place})}>
+          {placesItems}
         </Picker>
         <Button
             onPress={onButtonPress}
@@ -105,17 +101,91 @@ export default class FastGo extends Component {
 }
 
 
+function getRegions() {
+  return fetch('https://bigbang.skedgo.com/satapp-beta/regions.json', {
+            method: 'POST',
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json',
+              'X-TripGo-Key': '3ff9fc635afb6187ccf33ccc4610b80a' 
+            },
+            body: JSON.stringify({
+              v: '2',
+            })
+          })
+          .then((response) => response.json());
+}
+
+function computeFastestTrip(selectedMode, selectedPlaces) {
+  let promises = [];
+  let now = (Date.now() / 1000)
+  selectedPlaces.map((place, i) => {
+    promises.push(computeTrip(selectedMode, place.lat, place.lng));
+  });
+  return Promise.all(promises).then((routingJSONs => {
+    let faster = null;
+    let arrive = null;
+    routingJSONs.map((routingJSON, i) => {
+      newArrive = getFirstArrive(routingJSON, now);
+      if (faster == null || arrive > newArrive) {
+        faster = routingJSON
+        arrive = newArrive;
+        console.log("new faster one: " + newArrive)
+      }
+    })
+    return faster;
+  }))
+}
+
+function getFirstArrive(routingJSON, now) {
+  let arrive = null;
+  routingJSON.groups.map((group => {
+    group.trips.map((trip => {
+      if (trip.depart <= now) {
+        console.log('found one in the past')
+        return;
+      }
+      if (arrive === null || arrive > trip.arrive) {
+        console.log('found a faster: ' + trip.arrive)
+        arrive = trip.arrive;
+      }
+    }))
+  }))
+  return arrive;
+}
+
+function computeTrip(selectedMode, fromLat, fromLng) {
+  data = {
+    fromLoc: '(-33.894436,151.110030)',
+    toLoc: `(${fromLat},${fromLng})`,
+    mode: selectedMode,
+    wp: '(1,1,1,1)' 
+  }
+  let url = 'https://bigbang.skedgo.com/satapp-beta/routing.json'+ 
+            `?from=${data.fromLoc}&to=${data.toLoc}&modes=${data.mode}&wp=${data.wp}&v=11`
+  console.log(url)
+  return fetch(url, {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'X-TripGo-Key': '3ff9fc635afb6187ccf33ccc4610b80a' 
+      }
+    })
+    .then((response) => response.json())
+}
+
 function getModes(regionsJSON, regionName) {
   let modes = regionsJSON.modes;
   var result = new Array();
-  regionsJSON.regions.forEach(function(region) {
-    if (region.name.localeCompare(regionName) == 0) {
-      region.modes.forEach(function(mode) {
-        modes[mode].mode = mode;
-        result.push(modes[mode]);
-      });
-    }
-  });
-  // console.log(result);
+  regionsJSON.regions.map(region => {
+    if (region.name.localeCompare(regionName) != 0) 
+      return;
+    
+    region.modes.map(mode => {
+      modes[mode].mode = mode;
+      result.push(modes[mode]);
+    });
+  })  
   return result;
 }
